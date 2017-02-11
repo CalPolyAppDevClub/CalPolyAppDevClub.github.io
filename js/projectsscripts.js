7a0e1ce4f5fd3dc334af7190b003bd524f962719
Namjoon <eric_joo@ymail.com>
2017-02-10 19:30:23 -0800

    Update projectsscripts.js

--- js/projectsscripts.js
+++ js/projectsscripts.js
@@ -8,6 +8,15 @@ $.loadList = function () {
 
 function populate(data) {
    var table = document.getElementById("candidateList");
+   var row = table.insertRow(0);
+   var cell1 = row.insertCell(0);
+   var cell2 = row.insertCell(1);
+   var cell3 = row.insertCell(2);
+   var cell4 = row.insertCell(3);
+   cell1.innerHTML = "your name";
+   cell2.innerHTML = "your year";
+   cell3.innerHTML = "your major";
+   cell4.innerHTML = "<td><a href='#' class='btn'>Download Resume</a></td>";
    var i;
    for (i = 0; i < data.length; i++) {
       
