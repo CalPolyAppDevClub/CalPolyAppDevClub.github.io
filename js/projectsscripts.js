df31bca883f18e81e48a1c417eb31582c21f8503
Namjoon <eric_joo@ymail.com>
2017-02-10 20:06:53 -0800

    Update projectsscripts.js

--- js/projectsscripts.js
+++ js/projectsscripts.js
@@ -30,11 +30,12 @@ function writeCandidateData(candidateID, name, year, major) {
 }
 
 function onSubmitCandidateForm() {
-   var count;
+   var count = 1;
+   /*
    $.getJSON("https://polyappdev-projects.firebaseio.com/count.json", function (data) {
       count = data.num;
    });
-   
+   */
    writeCandidateData(toString(count + 1), document.getElementById("name").value, document.getElementById("year").value, document.getElementById("major").value);
    
    loadList();
