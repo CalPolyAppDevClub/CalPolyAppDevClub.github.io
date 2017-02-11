b18255c7273a84745e9d295b4462a2ee1948e390
Namjoon <eric_joo@ymail.com>
2017-02-10 19:43:08 -0800

    Update projectsscripts.js

--- js/projectsscripts.js
+++ js/projectsscripts.js
@@ -3,15 +3,6 @@ document.addEventListener('DOMContentLoaded', loadList, false);
 function loadList() {
    $.getJSON("https://polyappdev-projects.firebaseio.com/candidates.json", function (data) {
       var table = document.getElementById("candidateList");
-      var row = table.insertRow(0);
-      var cell1 = row.insertCell(0);
-      var cell2 = row.insertCell(1);
-      var cell3 = row.insertCell(2);
-      var cell4 = row.insertCell(3);
-      cell1.innerHTML = data[0][0];
-      cell2.innerHTML = data[0][1];
-      cell3.innerHTML = data[0][2];
-      cell4.innerHTML = "<td><a href='#' class='btn'>Download Resume</a></td>";
       var i;
       for (i = 0; i < data.length; i++) {
          var row = table.insertRow(0);
