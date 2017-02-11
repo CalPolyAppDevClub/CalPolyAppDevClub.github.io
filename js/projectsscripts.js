2576c2bdc8b75561b533f0a1394c9c03e5ccc5ff
Namjoon <eric_joo@ymail.com>
2017-02-10 19:29:10 -0800

    Update projectsscripts.js

--- js/projectsscripts.js
+++ js/projectsscripts.js
@@ -1,22 +1,13 @@
 document.addEventListener('DOMContentLoaded', loadList, false);
 
-function loadList() {
-   getJSON("https://polyappdev-projects.firebaseio.com/candidates.json", function (data) {
+$.loadList = function () {
+   $.getJSON("https://polyappdev-projects.firebaseio.com/candidates.json", function (data) {
       populate(data);
    });
-}
+};
 
 function populate(data) {
    var table = document.getElementById("candidateList");
-   var row = table.insertRow(0);
-   var cell1 = row.insertCell(0);
-   var cell2 = row.insertCell(1);
-   var cell3 = row.insertCell(2);
-   var cell4 = row.insertCell(3);
-   cell1.innerHTML = "your name";
-   cell2.innerHTML = "your year";
-   cell3.innerHTML = "your major";
-   cell4.innerHTML = "<td><a href='#' class='btn'>Download Resume</a></td>";
    var i;
    for (i = 0; i < data.length; i++) {
       
@@ -43,7 +34,7 @@ function writeCandidateData(candidateID, name, year, major) {
 function onSubmitCandidateForm() {
    writeCandidateData(1, document.getElementById("name").value, document.getElementById("year").value, document.getElementById("major").value);
    
-   //loadList();
+   loadList();
 
    document.getElementById('submit').scrollIntoView();
    
