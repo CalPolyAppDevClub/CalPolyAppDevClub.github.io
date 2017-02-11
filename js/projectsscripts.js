9ba20d7546925862d9a8db8f505640f1a3955e91
Namjoon <eric_joo@ymail.com>
2017-02-10 19:40:25 -0800

    Update projectsscripts.js

--- js/projectsscripts.js
+++ js/projectsscripts.js
@@ -14,15 +14,14 @@ function loadList() {
       cell4.innerHTML = "<td><a href='#' class='btn'>Download Resume</a></td>";
       var i;
       for (i = 0; i < data.length; i++) {
-
          var row = table.insertRow(0);
          var cell1 = row.insertCell(0);
          var cell2 = row.insertCell(1);
          var cell3 = row.insertCell(2);
          var cell4 = row.insertCell(3);
-         cell1.innerHTML = data[i].getElementById("name");
-         cell2.innerHTML = data[i].getElementById("year");
-         cell3.innerHTML = data[i].getElementById("major");
+         cell1.innerHTML = data[i][0];
+         cell2.innerHTML = data[i][1];
+         cell3.innerHTML = data[i][2];
          cell4.innerHTML = "<td><a href='#' class='btn'>Download Resume</a></td>";
       }
    });
